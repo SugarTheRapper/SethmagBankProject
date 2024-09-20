d191ff644e14e0bcb5862feb7b029068a5f458b5
SugarTheRapper <165840914+SugarTheRapper@users.noreply.github.com>
2024-09-20 08:48:44 +0200

    Add files via upload

---
+++ SethmagBankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sethmag_BankProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SethmagBankProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
